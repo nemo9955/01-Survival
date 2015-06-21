ab912e8b91a9211e8dcc11d27e3d5f1801f1ecad
nemo9955 <nemo9955@gmail.com>
2015-06-21 20:45:10 +0300

    initial project push

---
+++ android/src/com/nemo9955/survival10/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.nemo9955.survival10.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.nemo9955.survival10.Survival10;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Survival10(), config);
+	}
+}
---
+++ desktop/src/com/nemo9955/survival10/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nemo9955.survival10.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nemo9955.survival10.Survival10;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Survival10(), config);
+	}
+}
